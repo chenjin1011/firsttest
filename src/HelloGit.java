0ae41f6ddbf67eaec141c394bdc74cda2f5c7510
chenjin <chenjin1011@gmail.com>
2013-04-19 14:24:39 +0800

    test

---
+++ src/HelloGit.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: chenjin
+ * Date: 13-4-19
+ * Time: 下午2:22
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
